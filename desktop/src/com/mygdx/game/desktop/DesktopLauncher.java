a2f314e3780ade8d62d86611fb51f68f6038e002
Franco Román Meola <franco.meola@icloud.com>
2015-09-27 23:17:17 -0300

    Agrego una nueva escena con un modelo de una persona para empezar con la parte de animaciones. Comiteo también el .blend original por si hay que modificarlo.

--- desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -9,7 +9,7 @@ public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		new LwjglApplication(new MyGdxGame(), config);
-//        new LwjglApplication(new LoadModelsTest(), config);
+//        new LwjglApplication(new ModelTest(), config);
     }
 
 }
