481f6337e70709d6dbee60598d4e310e4be6776c
Franco Román Meola <franco.meola@icloud.com>
2015-10-01 21:11:16 -0300

    Cambios en la escena para que se noten las dos luces, en dos posiciones distintas. Se agregó una textura para el piso.

--- core/src/com/mygdx/game/GameElements.java
+++ core/src/com/mygdx/game/GameElements.java
@@ -2,10 +2,7 @@ package com.mygdx.game;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.assets.loaders.ModelLoader;
-import com.badlogic.gdx.graphics.Mesh;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.VertexAttribute;
-import com.badlogic.gdx.graphics.VertexAttributes;
+import com.badlogic.gdx.graphics.*;
 import com.badlogic.gdx.graphics.g3d.loader.ObjLoader;
 import com.badlogic.gdx.graphics.g3d.model.data.ModelData;
 import com.badlogic.gdx.math.Vector3;
@@ -38,23 +35,21 @@ public class GameElements {
          * Point Light
          */
         MyPointLight pointLight = new MyPointLight();
-        float[] pointLightPosition = new float[]{0f, 3f, 0f};
-        pointLight.setPosition(pointLightPosition);
-        pointLight.setAmbientLight(new float[]{0.5f,0.5f,0.5f,1f});
-        pointLight.setSpecularLight(new float[]{0f,1f,0f,1f});
-        pointLight.setLightColor(new float[]{0f,0f,1f,1f});
-        pointLight.setGlobalAmbientLight(new float[]{0f,0f,0f,1f});
+        pointLight.setPosition(new float[]{-1.5f, 3f, 0f});
+        pointLight.setAmbientLight(Color.WHITE);
+        pointLight.setSpecularLight(Color.GREEN);
+        pointLight.setLightColor(Color.BLUE);
+        pointLight.setGlobalAmbientLight(Color.BLACK);
         lights.add(pointLight);
         /**
          * Directional Light
          */
         MyDirectionalLight directionalLight = new MyDirectionalLight();
-        float[] directionalLightPosition = new float[]{0f, 3f, 0f};
-        directionalLight.setPosition(directionalLightPosition);
-        directionalLight.setAmbientLight(new float[]{0.5f,0.5f,0.5f,1f});
-        directionalLight.setSpecularLight(new float[]{0f,1f,0f,1f});
-        directionalLight.setLightColor(new float[]{0f,0f,1f,1f});
-        directionalLight.setGlobalAmbientLight(new float[]{0f,0f,0f,1f});
+        directionalLight.setPosition(new float[]{1.5f, -3f, 0f});
+        directionalLight.setAmbientLight(Color.BLACK);
+        directionalLight.setSpecularLight(Color.WHITE);
+        directionalLight.setLightColor(Color.WHITE);
+        directionalLight.setGlobalAmbientLight(Color.BLACK);
         lights.add(directionalLight);
         /**
          * Spot Light
@@ -89,7 +84,7 @@ public class GameElements {
     }
 
     public static DisplayableObject initQuad() {
-        Texture texture = new Texture("quad/textureQuad.png");
+        Texture texture = new Texture("quad/track.jpg");
         Material material = new Material();
         Mesh mesh = new Mesh(true, 4, 6, new VertexAttribute(VertexAttributes.Usage.Position, 4, "a_position"), new VertexAttribute(VertexAttributes.Usage.Normal, 3, "a_normal"), new VertexAttribute(VertexAttributes.Usage.TextureCoordinates, 2, "a_texCoord0"));
         mesh.setVertices(new float[] { //position, normal, texCoord
@@ -102,7 +97,7 @@ public class GameElements {
         material.setAmbient(new float[]{0.5f,0f,0f,1f});
         material.setSpecular(new float[]{0.5f,0f,0f,1f});
         material.setDiffuse(new float[]{0.5f,0f,0.2f,1f});
-        material.setShininess(0f);
+        material.setShininess(0.5f);
         return new DisplayableObject(mesh, new Vector3(0,0,0), texture, material);
     }
 
